204834ce28bfb0384fd27625d4276c0b76ccee42
s2 <s2@31337.it>
2016-11-10 15:23:38 +0100

    add remove button and bootstrap fonts

--- index.js
+++ index.js
@@ -6,6 +6,12 @@
 		$('#task-list').append('views/task-item.ejs', {title: $('#task').val()});
 	});
 
+	$(document).on('click', '.remove-task', function(ev) {
+		var task = $(ev.currentTarget).parent();
+		task.remove();
+	});
+
+
 	//initial page setup
 	$('.container').html('views/tasks.ejs', {});
 })(jQuery)
