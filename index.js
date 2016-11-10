834863523093dcbb82bd8547dfda0d8b06216baa
s2 <s2@31337.it>
2016-11-10 14:39:16 +0100

    add a task

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 (function($) {
-	//future code goes here
-})()
+	$(document).on('submit', '#add-task-form', function(ev) {
+		ev.preventDefault();
+		var newTask = '<li class="list-group-item">' + $('#task').val() + '</li>';
+		$('#task-list').append(newTask);
+	});
+})(jQuery)
